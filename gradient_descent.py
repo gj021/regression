95a30f163a653ddd525310e11f92a3909b3f6399
gj021 <f20160080@hyderabad.bits-pilani.ac.in>
2019-11-18 15:31:09 +0530

    gradient descent with test case

--- gradient_descent.py
+++ gradient_descent.py
@@ -25,6 +25,7 @@ class gradient_descent:
             error_g += self.w2*self.data[x,1]
             error_g -= self.data[x,2]
         return error_g
+
     def sumw1(self):
         error_g = self.w0*self.data.shape[0]
         for x in range(0,self.data.shape[0]):
@@ -33,6 +34,7 @@ class gradient_descent:
             error_g -= self.data[x,2]
             error_g = error_g*self.data[x,0]
             return error_g
+    
     def sumw2(self):
         error_g = self.w0*self.data.shape[0]
         for x in range(0,self.data.shape[0]):
@@ -45,31 +47,37 @@ class gradient_descent:
     def error_fn(self):
         error = 0
         for x in range(0,self.data.shape[0]):
-            dev = self.w0
-            dev += self.w1*self.data[x,0]
-            dev += self.w2*self.data[x,1]
-            dev -= self.data[x,2]
-            dev = dev*dev
-            dev = dev*0.5
-        error += dev
+            deviation = self.w0
+            deviation += self.w1*self.data[x,0]
+            deviation += self.w2*self.data[x,1]
+            deviation -= self.data[x,2]
+            deviation = deviation*deviation
+            deviation = deviation*0.5
+        error += deviation
         return error
 
     #matrix operation and updating w
     def w_approximation(self,error):
-        g0 = self.sumw0()
-        g1 = self.sumw1()
-        g2 = self.sumw2()
+        gradient0 = self.sumw0()
+        gradient1 = self.sumw1()
+        gradient2 = self.sumw2()
         new_error = self.error_fn()
         if(abs(error-new_error) < self.stoping_constant):
             return
         else:
             error = new_error
-        self.w0 = self.w0 - g0*self.learning_rate
-        self.w1 = self.w1 - g1*self.learning_rate
-        self.w2 = self.w2 - g2*self.learning_rate
+        self.w0 = self.w0 - gradient0*self.learning_rate
+        self.w1 = self.w1 - gradient1*self.learning_rate
+        self.w2 = self.w2 - gradient2*self.learning_rate
         print(self.w0, self.w1, self.w2, error)
         self.w_approximation(error)
 
 reg = gradient_descent(data)
 reg.w_approximation(0)
-#    self.data,self.learning_rate, self.stoping_constant,self.w0,self.w1,self.w1,self.w2 
+
+# enter w0:1
+# enter w1:1
+# enter w2:1
+# enter learning_rate:.000000001
+# enter stoping constant:1
+#0.6107846055673246 0.8784019746959085 0.26206522326523096 0.22571891015634002
